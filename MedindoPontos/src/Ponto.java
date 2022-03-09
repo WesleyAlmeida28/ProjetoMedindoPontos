330cecd51052826d13f69ddd272ac273b365a8a0
Wesley Brito <wesleyb.almeida@yahoo.com.br>
2022-03-08 22:01:10 -0300

    Construtores da classe criado

--- MedindoPontos/src/Ponto.java
+++ MedindoPontos/src/Ponto.java
@@ -1,4 +1,19 @@
 
 public class Ponto {
 
+	private int x;
+	private int y;
+	
+	// Construtor que recebe por parametro dois numeros inteiros x e y respectivamente.
+	public Ponto(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	//Construtor que não recebe nada por parametro mas seus valores são inicializados em 0
+	public Ponto() {
+		this.x = 0;
+		this.y = 0;
+	}
+
 }
